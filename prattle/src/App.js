0a76db0e59952865ffd1a5f4651928866b43bd60
B. Opao <brianopao@gmail.com>
2021-11-02 09:23:50 -0700

    Added router switching to App.js

--- prattle/src/App.js
+++ prattle/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/Home';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route path='/about' component={About} exact/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
