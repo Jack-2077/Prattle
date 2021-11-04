d4aba5167914784a863ce1c02ad03ac55d7953a7
Brian Opao <brianopao@hotmail.com>
2021-11-03 20:04:30 -0700

    Added Error.js for the error page not found

--- prattle/src/App.js
+++ prattle/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
+import Error from './pages/Error';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Switch>
         <Route path='/' component={Home} exact/>
         <Route path='/about' component={About} exact/>
+        <Route component={Error} exact/>
         </Switch>
       </BrowserRouter>
     </div>
