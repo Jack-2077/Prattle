d063b504a0e1b72bc5b5b334b20c0a4276e73023
Cezaraus <brianopao@gmail.com>
2021-11-05 19:33:34 -0700

    Updated Chatbox.js with hardcoded messages

--- prattle/src/components/Chatbox.js
+++ prattle/src/components/Chatbox.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import './chat.css';
 import { Typography } from '@mui/material';
+import Avatar from '@mui/material/Avatar'
+
+let messages =[
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+  "ut labore et dolore magna aliqua. Rhoncus dolor purus non enim",
+  "praesent elementum facilisis leo vel. Risus at ultrices mi tempus",
+  "imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.",
+  "imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus."
+]
+
+let displayMessage=[];
+for (var i = 0; i < messages.length; i++) {
+  displayMessage.push(
+    <div className='chat-bubble'>
+      <Avatar className="chat-avatar">A</Avatar>
+      <Typography paragraph className="chat-message">
+      {messages[i]}
+      </Typography>
+    </div> 
+  );
+}
 
 function Chatbox() {
     return (
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
+        <div className="chat-area">
+          {displayMessage}
+        </div>
       );
 }
 
