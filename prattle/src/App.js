b09d7b6d9d6c4471878fa34f4ba7e201c68c6b6c
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-11-15 15:23:21 -0800

    Revert back without css transitions

--- prattle/src/App.js
+++ prattle/src/App.js
@@ -4,28 +4,18 @@ import About from './pages/About';
 import Error from './pages/Error';
 import SignUp from './components/LoginPage/SignUp';
 import SignIn from './components/LoginPage/SignIn';
-import { Route, Switch, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  const location = useLocation();
   return (
-    <div className='page'>
-      <Route
-        render={() => (
-          <TransitionGroup>
-            <CSSTransition key={location.key} timeout={300} classNames='fade'>
-              <Switch location={location}>
-                <Route path='/' component={Home} exact />
-                <Route path='/about' component={About} />
-                <Route path='/signin' component={SignIn} />
-                <Route path='/signup' component={SignUp} />
-                <Route component={Error} />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )}
-      />
+    <div>
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/about' component={About} />
+        <Route path='/signin' component={SignIn} />
+        <Route path='/signup' component={SignUp} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
