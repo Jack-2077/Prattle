73263bc57c0543914c7395b8e27ac0f2c2d47fb7
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-11-15 14:17:46 -0800

    Add Transition Group package

--- prattle/src/App.js
+++ prattle/src/App.js
@@ -4,20 +4,28 @@ import About from './pages/About';
 import Error from './pages/Error';
 import SignUp from './components/LoginPage/SignUp';
 import SignIn from './components/LoginPage/SignIn';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 function App() {
+  const location = useLocation();
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/' component={Home} exact />
-          <Route path='/about' component={About} exact />
-          <Route path='/signin' component={SignIn} exact />
-          <Route path='/signup' component={SignUp} exact />
-          <Route component={Error} />
-        </Switch>
-      </BrowserRouter>
+    <div className='page'>
+      <Route
+        render={() => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={300} classNames='fade'>
+              <Switch location={location}>
+                <Route path='/' component={Home} exact />
+                <Route path='/about' component={About} />
+                <Route path='/signin' component={SignIn} />
+                <Route path='/signup' component={SignUp} />
+                <Route component={Error} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </div>
   );
 }
--- prattle/src/components/LoginPage/SignUp.js
+++ prattle/src/components/LoginPage/SignUp.js
@@ -2,8 +2,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -13,7 +11,6 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { Link as RouterLink } from 'react-router-dom';
-import { TransitionGroup, CSSTransititon } from 'react-transition-group';
 
 function Copyright(props) {
   return (
