5e10dc20b89da0700ed6a7157ff57330c5568af0
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-11-15 11:10:15 -0800

    Add Routing to SignIn and SignUp page

--- prattle/src/App.js
+++ prattle/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Error from './pages/Error';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import SignUp from './components/LoginPage/SignUp';
+import SignIn from './components/LoginPage/SignIn';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-        <Route path='/' component={Home} exact/>
-        <Route path='/about' component={About} exact/>
-        <Route component={Error} exact/>
+          <Route path='/' component={Home} exact />
+          <Route path='/about' component={About} exact />
+          <Route path='/signin' component={SignIn} exact />
+          <Route path='/signup' component={SignUp} exact />
+          <Route component={Error} />
         </Switch>
       </BrowserRouter>
     </div>
--- prattle/src/pages/Home.js
+++ prattle/src/pages/Home.js
@@ -1,43 +1,47 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Navbar from '../components/Navbar';
-import Appbar from '../components/Appbar';
-import Chatbox from '../components/Chatbox';
-import Textinput from '../components/Textinput';
+// import Box from '@mui/material/Box';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Toolbar from '@mui/material/Toolbar';
+// import Navbar from '../components/Navbar';
+// import Appbar from '../components/Appbar';
+// import Chatbox from '../components/Chatbox';
+// import Textinput from '../components/Textinput';
 
+import SignIn from '../components/LoginPage/SignIn';
 const drawerWidth = 240;
 
 export default function Home() {
   return (
     <div>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <Appbar />
-        <Navbar />
-        <Box
-          position='absolute'
-          marginLeft={`${drawerWidth}px`}
-          component='maibackground.defaultn'
-          sx={{ flexGrow: 1, bgcolor: '', p: 3 }}
-        >
-          <Toolbar />
-          <Chatbox />
-        </Box>
-      </Box>
-      <Box
-        position='fixed'
-        marginLeft={`${drawerWidth}px`}
-        sx={{
-          bottom: 0,
-          width: 1,
-          height: 0.15,
-          backgroundColor: 'background.default',
-        }}
-      >
-        <Textinput />
-      </Box>
+      <SignIn />
     </div>
+    // <div>
+    //   <Box sx={{ display: 'flex' }}>
+    //     <CssBaseline />
+    //     <Appbar />
+    //     <Navbar />
+    //     <Box
+    //       position='absolute'
+    //       marginLeft={`${drawerWidth}px`}
+    //       component='maibackground.defaultn'
+    //       sx={{ flexGrow: 1, bgcolor: '', p: 3 }}
+    //     >
+    //       <Toolbar />
+    //       <Chatbox />
+    //     </Box>
+    //   </Box>
+    //   <Box
+    //     position='fixed'
+    //     marginLeft={`${drawerWidth}px`}
+    //     sx={{
+    //       bottom: 0,
+    //       width: 1,
+    //       height: 0.15,
+    //       backgroundColor: 'background.default',
+    //     }}
+    //   >
+    //     <Textinput />
+    //   </Box>
+    // </div>
   );
 }
