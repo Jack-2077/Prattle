40484081be64490ab34f288ac0b979d961c80c08
Cezaraus <brianopao@gmail.com>
2021-11-23 18:55:23 -0800

    Added a redirect route after signin

--- prattle/src/App.js
+++ prattle/src/App.js
@@ -4,16 +4,22 @@ import About from './pages/About';
 import Error from './pages/Error';
 import SignUp from './components/LoginPage/SignUp';
 import SignIn from './components/LoginPage/SignIn';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import {useState} from 'react';
 
 function App() {
+  const [isLoggedIn, setLogIn] = useState(false);
   return (
     <div>
       <Switch>
-        <Route path='/' component={Home} exact />
+        <Route path='/' exact >
+          {isLoggedIn ? <Redirect to="/home" /> : 
+            <SignIn onClick={(bool) => setLogIn(bool)}/>}
+        </Route>  
+        <Route path='/home' component={Home} />
         <Route path='/about' component={About} />
-        {/* <Route path='/signin' component={SignIn} />
-        <Route path='/signup' component={SignUp} /> */}
+        {/* <Route path='/signin' component={SignIn} /> */}
+        <Route path='/signup' component={SignUp} />
         <Route component={Error} />
       </Switch>
     </div>
