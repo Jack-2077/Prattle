92c6b997861e6c58f97fa497707182f7d3c98145
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-11-29 18:53:48 -0800

    Add divs for news article

--- prattle/src/components/Articles.js
+++ prattle/src/components/Articles.js
@@ -1,217 +1,41 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import './Articles.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Articles() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=465543691c9444e69ac401dcb442d179'
+      )
+      .then((res) => {
+        setNews(res.data.articles);
+      })
+      .catch((error) => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
   return (
     <div>
       <div className='page-wrap'>
         <div className='article-sidebar'>
           <div className='article-grid'>
-            <article>
-              <img src='https://lipsum.app/id/1/1200x600' alt='' />
-              <div>
-                <h3>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                </h3>
-                <p>
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/2/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut
-                </h3>
-                <p>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                  occaecat cupidatat non proident, sunt in culpa qui officia
-                  deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/3/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                  qui officia deserunt
-                </h3>
-                <p>
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi
-                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                  nulla pariatur. mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/4/1200x600' alt='' />
-              <div>
-                <h3>
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi{' '}
-                </h3>
-                <p>
-                  incididunt ut labore et dolore magna aliqua. ut aliquip ex ea
-                  commodo consequat. Duis aute irure dolor in reprehenderit in
-                  voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                  qui officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/5/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                  ea commodo consequat.
-                </h3>
-                <p>
-                  labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat. Duis aute irure dolor in reprehenderit in
-                  voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                  qui officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/6/1200x600' alt='' />
-              <div>
-                <h3>Laboris nisi ut aliquip ex ea commodo consequat.</h3>
-                <p>
-                  {' '}
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/7/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </h3>
-                <p>
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate
-                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                  sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/8/1200x600' alt='' />
-              <div>
-                <h3> Duis aute irure dolor in reprehenderit</h3>
-                <p>
-                  {' '}
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                  nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id
-                  est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/9/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                  nulla pariatur.
-                </h3>
-                <p>
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                  enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/10/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </h3>
-                <p>
-                  {' '}
-                  adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate
-                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                  sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/11/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Minim veniam, quis nostrud exercitation ullamco laboris nisi
-                  ut aliquip ex ea commodo consequat.
-                </h3>
-                <p>
-                  elit, sed do eiusmod tempor incididunt ut labore et dolore
-                  magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate
-                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                  sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
-            <article>
-              <img src='https://lipsum.app/id/12/1200x600' alt='' />
-              <div>
-                <h3>
-                  {' '}
-                  Ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </h3>
-                <p>
-                  consectetur adipisicing elit, sed do eiusmod tempor incididunt
-                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                  ea commodo consequat. Duis aute irure dolor in reprehenderit
-                  in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-                  in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </article>
+            {news.map((item, i) => (
+              <article
+                href={item.url}
+                onClick={() => (window.location.href = `${item.url}`)}
+              >
+                <img src={item.urlToImage} alt='' />
+                <div>
+                  <h3> {item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+              </article>
+            ))}
           </div>
           <div className='sidebar'>
             <div className='sticky'>
@@ -220,7 +44,6 @@ function Articles() {
           </div>
         </div>
       </div>
-      [[[https://codepen.io/ahmetcadirci25/pen/GBOwdE]]]
     </div>
   );
 }
