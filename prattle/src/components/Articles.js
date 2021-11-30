ad7e750abe2b4a3671e387ec2733f1b1aa6da94e
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-11-29 17:01:52 -0800

    Create Articles page

---
+++ prattle/src/components/Articles.js
@@ -0,0 +1,228 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import './Articles.scss';
+
+function Articles() {
+  return (
+    <div>
+      <div className='page-wrap'>
+        <div className='article-sidebar'>
+          <div className='article-grid'>
+            <article>
+              <img src='https://lipsum.app/id/1/1200x600' alt='' />
+              <div>
+                <h3>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                </h3>
+                <p>
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/2/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut
+                </h3>
+                <p>
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/3/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt
+                </h3>
+                <p>
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/4/1200x600' alt='' />
+              <div>
+                <h3>
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi{' '}
+                </h3>
+                <p>
+                  incididunt ut labore et dolore magna aliqua. ut aliquip ex ea
+                  commodo consequat. Duis aute irure dolor in reprehenderit in
+                  voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/5/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                  ea commodo consequat.
+                </h3>
+                <p>
+                  labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  commodo consequat. Duis aute irure dolor in reprehenderit in
+                  voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/6/1200x600' alt='' />
+              <div>
+                <h3>Laboris nisi ut aliquip ex ea commodo consequat.</h3>
+                <p>
+                  {' '}
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/7/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </h3>
+                <p>
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                  sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/8/1200x600' alt='' />
+              <div>
+                <h3> Duis aute irure dolor in reprehenderit</h3>
+                <p>
+                  {' '}
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id
+                  est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/9/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur.
+                </h3>
+                <p>
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/10/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </h3>
+                <p>
+                  {' '}
+                  adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                  sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/11/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat.
+                </h3>
+                <p>
+                  elit, sed do eiusmod tempor incididunt ut labore et dolore
+                  magna aliqua. Ut enim ad minim veniam, quis nostrud
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                  sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+            <article>
+              <img src='https://lipsum.app/id/12/1200x600' alt='' />
+              <div>
+                <h3>
+                  {' '}
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </h3>
+                <p>
+                  consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                  ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                  in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </article>
+          </div>
+          <div className='sidebar'>
+            <div className='sticky'>
+              <img src='https://lipsum.app/id/13/600x1200' alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
+      [[[https://codepen.io/ahmetcadirci25/pen/GBOwdE]]]
+    </div>
+  );
+}
+
+export default Articles;
